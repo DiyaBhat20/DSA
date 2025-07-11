3fb50acc1256e94ac1246f236c31fe2818297ec9
Diya Bhat <diyabhat427@gmail.com>
2025-07-11 11:27:38 +0530

    FrogJump Code DP

--- FrogJump.cpp
+++ FrogJump.cpp
@@ -1,21 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
 class Solution {
 public:
-    int recFun(vector<int>& heights,int ind)
-    {
-        if(ind==0)
-        {
+    int recFun(vector<int>& heights, int ind) {
+        if (ind == 0) {
             return 0;
         }
-        int left=recFun(heights,ind-1)+(abs(heights[ind]-heights[ind-1]));
-        int right=INT_MAX;
-        if(ind>1)
-        {
-            right=recFun(heights,ind-2)+(abs(heights[ind]-heights[ind-2]));   
+        
+        int left = recFun(heights, ind - 1) + abs(heights[ind] - heights[ind - 1]);
+        
+        int right = INT_MAX;
+        if (ind > 1) {
+            right = recFun(heights, ind - 2) + abs(heights[ind] - heights[ind - 2]);
         }
-        return min(left,right);
+        
+        return min(left, right);
     }
 
     int frogJump(vector<int>& heights) {
-        return recFun(heights,heights.size());
+        int n = heights.size();
+        return recFun(heights, n - 1);
     }
 };
+
+int main() {
+    int n;
+    cout << "Enter the number of stones: ";
+    cin >> n;
+    
+    vector<int> heights(n);
+    cout << "Enter the heights of the stones:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> heights[i];
+    }
+    
+    Solution sol;
+    int minCost = sol.frogJump(heights);
+    cout << "Minimum total cost for the frog: " << minCost << endl;
+
+    return 0;
+}
